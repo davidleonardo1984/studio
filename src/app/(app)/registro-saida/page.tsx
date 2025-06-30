39b31a5d83f949131afe5b0647cf36b2bef939f9
Frequência Natural <frequencianatural17@gmail.com>
2025-06-30 14:44:08 +0000

    DESFAZER A ULTIMA SOLICITAÇÃO

--- src/app/(app)/registro-saida/page.tsx
+++ src/app/(app)/registro-saida/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import React, { useState, useEffect, useRef } from 'react';
@@ -191,12 +192,11 @@ export default function RegistroSaidaPage() {
         </h1>
       )}
       <Card className={cn(
-          "mx-auto shadow-xl",
-          isFocusMode ? "w-full" : "w-full max-w-lg"
+          "mx-auto shadow-xl w-full max-w-lg"
       )}>
         <CardHeader>
           <div className="flex justify-between items-center">
-              <div className={cn(isFocusMode && 'hidden')}>
+              <div>
                 <CardTitle className="text-2xl font-bold text-primary font-headline">Registro de Saída de Veículo</CardTitle>
                 <CardDescription>Insira o código de barras para registrar a saída.</CardDescription>
               </div>
