ddd45e17799164beed109d44d4d64d82b5c94a55
Frequência Natural <frequencianatural17@gmail.com>
2026-02-01 20:12:58 +0000

    Try fixing this error: `Console Error: [2026-02-01T20:11:30.092Z] @fireb

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -38,5 +38,20 @@ const app = isFirebaseConfigured && !getApps().length ? initializeApp(firebaseCo
 //   }
 // }
 const db = app ? getFirestore(app) : null;
+
+if (db) {
+  enableIndexedDbPersistence(db)
+    .catch((err) => {
+      if (err.code == 'failed-precondition') {
+        // This can happen if you have multiple tabs open.
+        // Persistence can only be enabled in one tab at a time.
+        console.warn("Firestore persistence failed: can only be enabled in one tab at a time.");
+      } else if (err.code == 'unimplemented') {
+        // The current browser does not support all of the
+        // features required to enable persistence
+        console.warn("Firestore persistence is not supported in this browser.");
+      }
+    });
+}
 
 export { app, db, isFirebaseConfigured };
